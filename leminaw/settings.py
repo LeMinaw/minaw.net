df2fc84ddfdf83ca56d25c374709f78f130ab028
LeMinaw <leminaw@gmail.com>
2017-07-17 01:49:45 +0200

    fix statics

--- leminaw/settings.py
+++ leminaw/settings.py
@@ -121,7 +121,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
