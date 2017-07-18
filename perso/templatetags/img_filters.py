444d09d040c9f776ae2f69add2b568b05873b57c
LeMinaw <leminaw@gmail.com>
2017-07-18 21:02:39 +0200

    catch non existence of cover

--- perso/templatetags/img_filters.py
+++ perso/templatetags/img_filters.py
@@ -12,7 +12,10 @@ def img_repl(tag_match):
     reg = re.compile(r'^\[img\]([^|]+)\|?([^|]+)?\[\/img\]$')
     cover_name = re.match(reg, tag_match.group(0)).group(1)
     css_class = re.match(reg, tag_match.group(0)).group(2)
-    cover = Cover.objects.get(name=cover_name)
+    try:
+        cover = Cover.objects.get(name=cover_name)
+    except Cover.DoesNotExist:
+        return "<p>Erreur : %s n'existe pas.</p>" % cover_name
     return "<img src=\"%s\" alt=\"%s\" class=\"%s\"/>" % (cover.image.url, cover.descr, css_class)
 
 
