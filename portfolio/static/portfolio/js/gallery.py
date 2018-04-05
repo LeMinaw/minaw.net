8b47a97cd3fff0ab9988616d3665e5a28457fdac
LeMinaw <leminaw@gmail.com>
2018-04-05 17:26:50 +0200

    gallery widget update

--- portfolio/static/portfolio/js/gallery.py
+++ portfolio/static/portfolio/js/gallery.py
@@ -1,35 +1,35 @@
 __pragma__('alias', 'S', '$')
 
 def total_width(elems):
-    """Cumulated width of `elems`, including margins."""
-    return sum([S(el).outerWidth(True) for el in elems])
-
-def total_blank(elems):
-    """Cumulated width difference between outer and inner widths of `elems`."""
-    return total_width(elems) - sum([S(el).innerWidth() for el in elems])
+    """Cumulated width of <elems>."""
+    return sum([el.offsetWidth for el in elems])
 
 def empty(elem):
-    """Removes all child nodes of `elem`."""
+    """Removes all child nodes of <elem>."""
     while elem.firstChild is not None:
         elem.removeChild(elem.firstChild)
 
 def avg(iterable):
-    """Average value of `iterable` members."""
+    """Average value of <iterable>."""
     return sum(iterable) / len(iterable)
 
-def parse_px(val):
-    """Parse px string value: '45px' -> 45"""
+def from_px(val):
+    """Read int from px string value: '45px' -> 45"""
     return int(val[:-2])
+
+def to_px(val):
+    """Returns px string value from int: 45 -> '45px'"""
+    return f"{val}px"
 
 class Gallery:
     def __init__(self, pics):
         self.pics = pics
         self.parent = pics[0].parentNode
-        self.init_widths = [S(pic).css("width") for pic in pics]
+        self.init_widths = [pic.offsetWidth for pic in pics]
 
     def reset_widths(self):
         for pic, w in zip(self.pics, self.init_widths):
-            S(pic).css("width", w)
+            pic.style.width = to_px(w)
 
     def draw(self, rows_nb):
         self.reset_widths()
@@ -49,9 +49,9 @@ class Gallery:
         for row, width in zip(self.rows, widths):
             ratio = avg_width / width
             for pic in row:
-                w = parse_px(S(pic).css("width"))
+                w = from_px(S(pic).css("width"))
                 w *= ratio
-                S(pic).css("width", w)
+                pic.style.width = to_px(w)
 
         # Display rows
         empty(self.parent)
@@ -59,6 +59,10 @@ class Gallery:
             for pic in row:
                 S(self.parent).append(pic)
             S(self.parent).append("<br/>")
+
+        # Set container width
+        self.parent.style.width = to_px(avg_width + 1) # Avoids rounding problems
+        print(to_px(avg_width))
 
     def destroy(self):
         self.reset_widths()
