6276ccabf7c98fe08ca81590c0570daeb9c90e2f
LeMinaw <leminaw@gmail.com>
2019-06-17 14:03:28 +0200

    update admin headers

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from portfolio.models import Work, Category
 
 
+admin.site.site_title = "Admin minaw.net"
+admin.site.site_header = "Administration des services minaw.net"
+admin.site.index_title = "Index de l'administration"
+
+
 class WorkAdmin(admin.ModelAdmin):
     list_display        = ('__str__', 'pin', 'bck', 'slug', 'added')
     list_filter         = ('categ', 'pin', 'bck')
