71590a39cb3b163894b82805f7f50c9c48ca0659
LeMinaw <leminaw@gmail.com>
2019-12-02 04:22:26 +0100

    color palette logic improvements

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,22 +4,24 @@ from django.db.models import (Model, DateTimeField, SlugField,
 from django.urls import reverse
 from colorthief import ColorThief
 from .utils import rgb_to_hex, hex_to_rgb, alpha
+from PIL import Image
+
 
 class Work(Model):
     added     = DateTimeField(auto_now_add=True,                          verbose_name="ajouté")
     modif     = DateTimeField(auto_now=True,                              verbose_name="modifié")
     slug      = SlugField(unique=True,                                    verbose_name="identifiant")
     title     = CharField(max_length=64,                                  verbose_name="titre")
-    subtitle  = CharField(max_length=128,                     blank=True, verbose_name="sous-titre")
+    subtitle  = CharField(max_length=128,            blank=True,          verbose_name="sous-titre")
     cover     = ImageField(width_field='cover_w', height_field='cover_h', verbose_name="couverture")
-    cover_w   = PositiveSmallIntegerField(null=True,                      verbose_name="largeur couverture")
-    cover_h   = PositiveSmallIntegerField(null=True,                      verbose_name="hauteur couverture")
-    content   = TextField(                  blank=True,                   verbose_name="contenu")
+    cover_w   = PositiveSmallIntegerField(null=True, blank=True,          verbose_name="largeur couverture")
+    cover_h   = PositiveSmallIntegerField(null=True, blank=True,          verbose_name="hauteur couverture")
+    content   = TextField(                           blank=True,          verbose_name="contenu")
     bck       = BooleanField(default=False,                               verbose_name="arrière-plan")
     pin       = BooleanField(default=False,                               verbose_name="épinglé")
     black_txt = BooleanField(default=False,                               verbose_name="texte noir")
-    categ     = ManyToManyField('Category', blank=True,                   verbose_name="catégorie")
-    _palette  = CharField(max_length=64,    blank=True,                   verbose_name="palette")
+    categ     = ManyToManyField('Category',          blank=True,          verbose_name="catégorie")
+    _palette  = CharField(max_length=64,             blank=True,          verbose_name="palette")
 
     def __str__(self):
         return self.title
@@ -29,30 +31,28 @@ class Work(Model):
 
     def get_svg_placeholder(self):
         if self.cover_w is None or self.cover_h is None:
-            self.save() # To generate image dimensions
+            self.save(compute_palette=False) # To generate image dimensions
         w, h = self.cover_w, self.cover_h
         return ('data:image/svg+xml,'
             '<svg xmlns="http://www.w3.org/2000/svg" '
             f'viewBox="0 0 {w} {h}"></svg>')
-        # return ('data:image/svg+xml,'
-        #     '%3Csvg xmlns="http://www.w3.org/2000/svg" '
-        #     f'viewBox="0 0 {w} {h}"%3E%3C/svg%3E')
     
     def compute_color_palette(self):
-        ct = ColorThief(self.cover)
-        ct.image = ct.image.resize((100, 100))
+        self.cover.open() # Fixes a bug in file handling due to w/h update
+        thumb = Image.open(self.cover).resize((100, 100))
+        ct = ColorThief(thumb, is_obj=True)
         try:
-            cols = ct.get_palette(3, quality=50)
+            cols = ct.get_palette(3, ignore_black=False, quality=50)
         except Exception: # NoQA
             cols = ((255, 255, 255), (200, 200, 200), (220, 220, 220))
         while len(cols) < 3:
             cols.append(cols[0])
         self._palette = ';'.join([str(rgb_to_hex(col[:3])) for col in cols])
-        # super().save()
     
     def get_palette(self):
         if not self._palette:
-            self.save() # To generate color palette
+            self.compute_color_palette()
+            self.save(compute_palette=False) # To generate color palette
         palette = self._palette.split(';')
         return tuple([hex_to_rgb(hexcode) for hexcode in palette])
     
@@ -63,8 +63,9 @@ class Work(Model):
             f'linear-gradient(120deg, {alpha(cols[1], 0.8)}, {alpha(cols[1], 0.2)} 60%),'
             f'linear-gradient(240deg, {alpha(cols[2], 0.8)}, {alpha(cols[2], 0.2)} 60%);')
     
-    def save(self, *args, **kwargs):
-        self.compute_color_palette()
+    def save(self, compute_palette=True, *args, **kwargs):
+        if compute_palette:
+            self.compute_color_palette()
         super().save(*args, **kwargs)
 
     class Meta:
